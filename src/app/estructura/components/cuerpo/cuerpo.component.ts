6c2d75e5edd0dce59cc1fd3f9a0ede5d8c1ecfc8
enriquemolina15 <molinfernandez15@gmail.com>
2023-06-09 09:43:40 +0200

    primero

---
+++ src/app/estructura/components/cuerpo/cuerpo.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuerpo',
+  templateUrl: './cuerpo.component.html',
+  styleUrls: ['./cuerpo.component.css']
+})
+export class CuerpoComponent {
+  public listadoHeroes: Array<any>
+ constructor(){
+  this.listadoHeroes = [
+    {
+      nombre: "Superman",
+      imagen: "",
+      ataque: 80,
+      defensa: 93,
+      poder: 350,
+      vida: 100
+    },
+    {
+      nombre: "Batman",
+      imagen: "",
+      ataque: 60,
+      defensa: 75,
+      poder: 50,
+      vida: 100
+    },
+    {
+      nombre: "Spiderman",
+      imagen: "",
+      ataque: 86,
+      defensa: 89,
+      poder: 50,
+      vida: 100
+    },
+    {
+      nombre: "Linterna Verde",
+      imagen: "",
+      ataque: 82,
+      defensa: 83,
+      poder: 200,
+      vida: 100
+    },
+    {
+      nombre: "Ironman",
+      imagen: "",
+      ataque: 83,
+      defensa: 85,
+      poder: 150,
+      vida: 100
+    },
+    {
+      nombre: "Capitán America",
+      imagen: "",
+      ataque: 64,
+      defensa: 95,
+      poder: 135,
+      vida: 100
+    },
+    {
+      nombre: "Thor",
+      imagen: "",
+      ataque: 92,
+      defensa: 75,
+      poder: 275,
+      vida: 100
+    }
+
+  ];
+}
+ 
+  
+  public hayHeroes(): boolean {
+    return this.listadoHeroes.length > 0;
+  }
+   
+}
